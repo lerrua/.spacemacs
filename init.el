a19a2adf3a052ef9a251869e5d061e864a9e7db5
Igor P. Leroy <ip.leroy@gmail.com>
2015-10-08 15:02:10 -0300

    Add eyebrowse for better emacs workspaces

--- init.el
+++ init.el
@@ -43,6 +43,7 @@ values."
      syntax-checking
      version-control
      osx
+     eyebrowse
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
